20456a1fa5b4a2b86dead8b2952b36d652712ac3
name <email>
2024-06-10 02:57:51 +0530

    final

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
-soon!!
+
+# Instagram Report Bot v5.
+
+This py code can report any content on Instagram.
+Works within seconds.
+This is the 5th re-code of this project. We've tried to make it even better from it's previous version in an updated readable format.
+
+## 🛠 Languages
+Python. 100%.
+## Features
+
+- Fast reports
+- Understandable UI
+- Proxy supported
+- Can be made cross platform
+
+
+## Screenshot :
+
+![App Screenshot](https://media.discordapp.net/attachments/1111906830633943171/1249474609049501726/image.png?ex=66676f68&is=66661de8&hm=4a57857550b98e0b7eed6dc1d52f2ccb7ba0f4dadbcb4c8901e1620e24a55ce9&=&format=webp&quality=lossless)
+
+
+## Installation Guide
+
+To install :
+
+```bash
+  git clone https://github.com/arnavos/IGSpamv5
+  cd https://github.com/arnavos/IGSpamv5
+```
+    
+## Support
+
+For support, email antiabuse@firemail.cc .
+
+## FAQ
+
+#### Does it work on proxies?
+
+- Yes, but you can use it without proxies too.
+
+#### How likely are the chances of getting the content taken down from instagram?
+
+- This code works on reporting in forms so there's a high chance of getting a positive change if your report is really worth it.
+
+
+## Contributing
+
+Contributions are always welcome!
+
+Please adhere to this project's `code of conduct` and keep in mind that we do not promote any illegal or malicious activity through this project and this is solely only for educational purposes.
+
+
+## Feedbacks
+
+If you have any feedback, please reach out to us at https://discord.gg/emoney.
+
+
+## Authors
+
+- [@arnavos](https://www.github.com/arnavos)
+- [@david1337x](https://www.github.com/david1337x)
+
+
+## 🔗 Links
+[![arnav's instagram](https://img.shields.io/badge/Instagram-E4405F?style=for-the-badge&logo=instagram&logoColor=white)](https://instagram.com/arnxvfr)
+[![david's instagram](https://img.shields.io/badge/Instagram-E4405F?style=for-the-badge&logo=instagram&logoColor=white)](https://instagram.com/_davidoesitbetter)
+
--- libs/logo.py
+++ libs/logo.py
@@ -8,29 +8,9 @@ logo = """
      |  | |  |  |\__  ||_|  |_||     |\__  ||   _/|     ||  \_/  |
      |  | |  |  |/  \ |  |  |  |  _  |/  \ ||  |  |  _  ||   |   |
      |  | |  |  |\    |  |  |  |  |  |\    ||  |  |  |  ||   |   |
-    |____||__|__| \___|  |__|  |__|__| \___||__|  |__|__||___|___|v4"""
-
-youtube_urls = [
-    "Süpriz - https://www.youtube.com/watch?v=G0STVKbQEaQ",
-    "Babaların İnfazı - https://www.youtube.com/watch?v=DmkoBPxyyeo",
-    "Polat Ve Testere Türk Ruleti - https://www.youtube.com/watch?v=GPa85nu0-ho",
-    "Biat Edin - https://www.youtube.com/watch?v=GFCczMrKa28",
-    "Kötü Köpek Sürüye Kurt Getirdi - https://www.youtube.com/watch?v=tNYF-g1u5_Q",
-    "Sadece Ölüler Görür - https://www.youtube.com/watch?v=N_1l_au1nJA",
-    "Seyfo Dayı, Çakır ve Polat'a Alemi Anlatıyor - https://www.youtube.com/watch?v=0YarjcwVyqs",
-    "Memati Cerrahpaşalıların mekanında - https://www.youtube.com/watch?v=6TdIQ6gO1fM",
-    "Laz Ziya Testere Necmi'nin Mekanını Basıyor - https://www.youtube.com/watch?v=qeP8ZxXW8tc",
-    "Laz Ziya'dan Baron'a Racon - https://www.youtube.com/watch?v=qZiydYE4dUo",
-    "Süleyman Çakır ''Erkek adam küpe takmaz'' - https://www.youtube.com/watch?v=Q7WpI6Ekx84",
-    "Polat ve Çakır Tombalacıya Posta Koyuyor - https://www.youtube.com/watch?v=UxXEkXvgFb0",
-    "Tombalacının İnfazı - https://www.youtube.com/watch?v=VmIDGYLsucU",
-    "Abuzer Kömürcü Tüm Sahneler - https://www.youtube.com/watch?v=xTJav-ar6IQ",
-    "Seyfo Dayı - https://www.youtube.com/watch?v=8JJa4gXkD-Y"
-    ]
+    |____||__|__| \___|  |__|  |__|__| \___||__|  |__|__||___|___| v5"""
 
 def print_logo():
     print(Fore.RED + Style.BRIGHT + logo + Style.RESET_ALL + Style.BRIGHT +"\n")
-    print(Fore.MAGENTA + "      Yapımcı: Hichigo THT"+ Style.RESET_ALL + Style.BRIGHT)
-    print(Fore.CYAN + "\n", "-> Telegramdan fikri veren ve aynı zamanda söven arkadaşa teşekkür ediyorum.")
-    print ("\n", "-> Gaz maskesiyle gül koklamayanalara özel:\n    " + choice(youtube_urls))
+    print(Fore.MAGENTA + "      Developers: @arnavos and @david1337x"+ Style.RESET_ALL + Style.BRIGHT)
     print(Style.RESET_ALL + Style.BRIGHT, Style.BRIGHT)
